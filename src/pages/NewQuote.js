1a98f62ef9adcd028bb6ee5849106a2d3d62d9f6
Shubham Singh <shubham2101@gmail.com>
2022-08-22 23:24:43 +0530

    Imperative Navigation new quote to All quotes

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,12 @@
-import QuoteForm from "../components/quotes/QuoteForm"
+import { useHistory } from "react-router-dom";
+import QuoteForm from "../components/quotes/QuoteForm";
 const NewQuote = () => {
-    const addQuoteHandler = (quoteData) => {
-        console.log(quoteData)
-    }
-    return (
-        <QuoteForm onAddQuote={addQuoteHandler}/>
-    )
-}
+  const history = useHistory();
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+    history.push("/quotes");
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
+};
 
-export default NewQuote
+export default NewQuote;
