daef6a63cd73b661c0b7943c0872717c96e1fdc9
Shubham Singh <shubham2101@gmail.com>
2022-08-23 23:31:05 +0530

    conditionally rendering a nested route

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,21 +1,28 @@
-import {Route, useParams } from "react-router-dom";
-import Comments from "../components/comments/Comments"
-import HighlightedQuote from "../components/quotes/HighlightedQuote"
+import { Route, Link, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 const DUMMY_QUOTES = [
-  {id : "q1", author : "Shubham", text : "Life is beautiful."},
-  {id : "q2", author : "Rancho", text : "All is well."}
-]
+  { id: "q1", author: "Shubham", text: "Life is beautiful." },
+  { id: "q2", author: "Rancho", text: "All is well." },
+];
 const QuotesDetails = () => {
   const params = useParams();
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quotesId)
-  if(!quote) {
-    return <p>No Quotes Found!</p>
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quotesId);
+  if (!quote) {
+    return <p>No Quotes Found!</p>;
   }
   return (
     <section>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${quote.id}`} exact>
+        <div className="centered">
+          <Link to={`/quotes/${quote.id}/comments`} className="btn--flat">
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path="/quotes/:quotesId/comments">
-        <Comments/>
+        <Comments />
       </Route>
     </section>
   );
